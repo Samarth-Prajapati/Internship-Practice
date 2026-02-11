436b9a0513ae6f779709ab66eaad486c3db051dc
Samarth-Prajapati <samarthp475@gmail.com>
2026-02-11 16:41:31 +0530

    Added : View all documents, update documents and delete documents.

--- 11022026_wednesday/mongodb/main.py
+++ 11022026_wednesday/mongodb/main.py
@@ -65,6 +65,36 @@ def find_document(collection, data):
 
     return result
 
+def all_documents(collection):
+    result = collection.find()
+    if not result:
+        print("Document Not Found")
+        return None
+
+    for person in result:
+        print(f"Name : {person["name"]}, Role : {person["role"]}, Skills : {person["skills"]}")
+    return "Fetched Successfully"
+
+def delete_document(collection, data):
+    data_delete = find_document(collection, data)
+    if not data_delete:
+        print("Document Not Found")
+        return None
+
+    collection.delete_one(data)
+    return "Deleted Successfully"
+
+def update_document(collection, identifier, updated_data):
+    updated = collection.update_one(
+        identifier,
+        {"$set" : updated_data}
+    )
+    if not updated:
+        print("Document Not Found")
+        return None
+
+    return updated
+
 mongo = setup_mongodb()
 db = create_database(mongo)
 table = create_collection(db)
@@ -92,5 +122,61 @@ data3 = {
     "name" : "Samarth Prajapati",
 }
 
-find_data = find_document(table, data3)
-print(find_data)
+# find_data = find_document(table, data3)
+# print(find_data)
+
+data4 = {
+    "name" : "Om Mishra",
+    "role" : "AI Intern",
+    "skills" : ["Python", "AI"]
+}
+
+data5 = {
+    "name" : "Alok Yadav",
+    "role" : "AI Intern",
+    "skills" : ["Python", "AI"]
+}
+
+data6 = {
+    "name" : "Harsh Mistry",
+    "role" : "AI Intern",
+    "skills" : ["Python", "AI"]
+}
+
+data7 = {
+    "name" : "Kamlesh Patel",
+    "role" : "AI Intern",
+    "skills" : ["Python", "AI"]
+}
+
+data8 = {
+    "name" : "Yash Maisuriya",
+    "role" : "AI Intern",
+    "skills" : ["Python", "AI"]
+}
+
+# document3 = add_document(table, data4)
+# document4 = add_document(table, data5)
+# document5 = add_document(table, data6)
+# document6 = add_document(table, data7)
+# document7 = add_document(table, data8)
+
+# all_docs = all_documents(table)
+# print(all_docs)
+
+data9 = {
+    "name" : "Yash Maisuriya",
+}
+
+# del_document = delete_document(table, data9)
+# print(del_document)
+
+identifier_data = {
+    "name" : "Alok Yadav"
+}
+
+update_data = {
+    "role" : "BA"
+}
+
+update = update_document(table, identifier_data, update_data)
