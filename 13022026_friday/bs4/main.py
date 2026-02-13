29947a880200ef9ccf4be4ea6810bacc2f7d755e
Samarth-Prajapati <samarthp475@gmail.com>
2026-02-13 12:05:44 +0530

    Update : Added a function that returns all data from a single tag

--- 13022026_friday/bs4/main.py
+++ 13022026_friday/bs4/main.py
@@ -1,9 +1,39 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = "https://beautiful-soup-4.readthedocs.io/en/latest/"
+def load_bs4(url):
+    """
+    Parameters
+    ----------
+    url - Website URL for scrapping
 
-response = requests.get(url).content
+    Returns - soup
+    -------
+    """
 
-soup = BeautifulSoup(response, "html.parser")
-print(soup.prettify())
+    response = requests.get(url).content
+    bs4 = BeautifulSoup(response, "html.parser")
+    return bs4
+
+def load_all_data(bs4, tag):
+    """
+
+    Parameters
+    ----------
+    bs4 - BeautifulSoup object
+    tag - Tag object to be scraped
+
+    Returns - tag data
+    -------
+    """
+
+    tags_data = bs4.find_all(tag)
+    return tags_data
+
+soup = load_bs4("https://beautiful-soup-4.readthedocs.io/en/latest/")
+# print(soup.prettify())
+
+h1_tag = load_all_data(soup, "h1")
+
+for i in h1_tag:
+    print(i.text)
