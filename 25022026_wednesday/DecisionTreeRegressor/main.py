e3c94ea6c7effb84b12b6610010fd1da69afbbf3
Samarth-Prajapati <samarthp475@gmail.com>
2026-02-25 14:26:48 +0530

    Add : Load dataset and preprocessing.

---
+++ 25022026_wednesday/DecisionTreeRegressor/main.py
@@ -0,0 +1,67 @@
+import pandas as pd
+
+class DecisionTreeRegressor:
+
+    def __init__(self):
+        self.df = None
+
+    def load_dataset(self):
+        """
+        Loading the dataset.
+        Returns - None
+        -------
+        """
+
+        self.df = pd.read_csv("insurance.csv")
+        print(f"DataFrame = \n\n{self.df.head()}")
+
+        print("\n[INFO] Dataset Loaded Successfully.")
+
+    def check_dataset(self):
+        """
+        Checking the dataset.
+        Returns - None
+        -------
+        """
+
+        print("DataFrame Info = \n\n")
+        self.df.info()
+
+        print(f"\nDataFrame Description = \n\n{self.df.describe()}\n")
+
+        print(f"Number of Duplicates in DataFrame = {self.df.duplicated().sum()}")
+
+        print("\n[INFO] Dataset Checked Successfully.")
+
+    def drop_duplicates(self):
+        """
+        Dropping duplicates.
+        Returns - None
+        -------
+        """
+
+        self.df.drop_duplicates(inplace = True)
+        print(f"Number of Duplicates in DataFrame after Dropping Duplicates = {self.df.duplicated().sum()}")
+
+        print("\n[INFO] Duplicates Dropped Successfully.")
+
+    def eda(self):
+        pass
+
+def main():
+    print("=========================================== DECISION TREE REGRESSOR ===========================================")
+    regressor = DecisionTreeRegressor()
+
+    print("\n================================================= LOAD DATASET ================================================\n")
+    regressor.load_dataset()
+
+    print("\n================================================ CHECK DATASET ================================================\n")
+    regressor.check_dataset()
+
+    print("\n=============================================== DROP DUPLICATES ===============================================\n")
+    regressor.drop_duplicates()
+
+    print("\n===================================================== EDA =====================================================\n")
+
+if __name__ == "__main__":
+    main()
