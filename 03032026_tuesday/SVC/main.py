34e94ffb76772f49157b43676bd3f09226121574
Samarth-Prajapati <samarthp475@gmail.com>
2026-03-03 12:06:02 +0530

    Change : Added try except block.

--- 03032026_tuesday/SVC/main.py
+++ 03032026_tuesday/SVC/main.py
@@ -36,10 +36,16 @@ class SupportVectorClassifier:
         -------
         """
 
-        self.df = pd.read_csv(self.path)
-        print(f"\nDataset = \n{self.df.head()}\n")
+        try:
+            self.df = pd.read_csv(self.path)
+            print(f"\nDataset = \n{self.df.head()}\n")
 
-        print("Data Loaded Successfully.", end = seperator)
+            print("Data Loaded Successfully.", end = seperator)
+
+        except FileNotFoundError:
+            print(".csv file not found.")
+        except Exception as error:
+            print(error)
 
     def check_data(self):
         """
@@ -65,10 +71,14 @@ class SupportVectorClassifier:
         -------
         """
 
-        self.df.drop("user_id", axis = 1, inplace = True)
-        print(f"Dataset = \n{self.df.head()}\n")
+        try:
+            self.df.drop("user_id", axis = 1, inplace = True)
+            print(f"Dataset = \n{self.df.head()}\n")
 
-        print("Data Preprocessed Successfully.", end = seperator)
+            print("Data Preprocessed Successfully.", end = seperator)
+
+        except Exception as error:
+            print(error)
 
     def eda(self):
         """
@@ -77,27 +87,31 @@ class SupportVectorClassifier:
         -------
         """
 
-        corr = self.df.corr(numeric_only = True)
-        sns.heatmap(corr, annot = True)
-        plt.show()
+        try:
+            corr = self.df.corr(numeric_only = True)
+            sns.heatmap(corr, annot = True)
+            plt.show()
 
-        sns.pairplot(self.df, hue = "purchased")
-        plt.show()
+            sns.pairplot(self.df, hue = "purchased")
+            plt.show()
 
-        cols = ["age", "estimated_salary", "purchased"]
-        for i in range(len(cols)):
-            plt.subplot(1, 3, i + 1)
-            sns.boxplot(y = self.df[cols[i]])
-            plt.title(cols[i])
-        plt.show()
+            cols = ["age", "estimated_salary", "purchased"]
+            for i in range(len(cols)):
+                plt.subplot(1, 3, i + 1)
+                sns.boxplot(y = self.df[cols[i]])
+                plt.title(cols[i])
+            plt.show()
 
-        for i in range(len(cols) - 1):
-            plt.subplot(1, 2, i + 1)
-            sns.histplot(self.df[cols[i]], kde = True)
-            plt.title(cols[i])
-        plt.show()
+            for i in range(len(cols) - 1):
+                plt.subplot(1, 2, i + 1)
+                sns.histplot(self.df[cols[i]], kde = True)
+                plt.title(cols[i])
+            plt.show()
 
-        print("EDA performed Successfully.", end = seperator)
+            print("EDA performed Successfully.", end = seperator)
+
+        except Exception as error:
+            print(error)
 
     def split_data(self):
         """
@@ -106,12 +120,16 @@ class SupportVectorClassifier:
         -------
         """
 
-        self.X = self.df.iloc[:, :-1]
-        self.y = self.df.iloc[:, -1]
+        try:
+            self.X = self.df.iloc[:, :-1]
+            self.y = self.df.iloc[:, -1]
 
-        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y, test_size = self.test_size, random_state = self.random_state)
+            self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y, test_size = self.test_size, random_state = self.random_state)
 
-        print("Data Splitting Successful.", end = seperator)
+            print("Data Splitting Successful.", end = seperator)
+
+        except Exception as error:
+            print(error)
 
     def transform_data(self):
         """
@@ -120,13 +138,17 @@ class SupportVectorClassifier:
         -------
         """
 
-        self.X_train["gender"] = self.encoder.fit_transform(self.X_train["gender"])
-        self.X_test["gender"] = self.encoder.transform(self.X_test["gender"])
+        try:
+            self.X_train["gender"] = self.encoder.fit_transform(self.X_train["gender"])
+            self.X_test["gender"] = self.encoder.transform(self.X_test["gender"])
 
-        self.X_train = self.scaler.fit_transform(self.X_train)
-        self.X_test = self.scaler.transform(self.X_test)
+            self.X_train = self.scaler.fit_transform(self.X_train)
+            self.X_test = self.scaler.transform(self.X_test)
 
-        print("Data Transformation Successful.", end = seperator)
+            print("Data Transformation Successful.", end = seperator)
+
+        except Exception as error:
+            print(error)
 
     def train_model(self):
         """
@@ -135,9 +157,13 @@ class SupportVectorClassifier:
         -------
         """
 
-        self.model.fit(self.X_train, self.y_train)
+        try:
+            self.model.fit(self.X_train, self.y_train)
 
-        print("Model Trained Successfully.", end = seperator)
+            print("Model Trained Successfully.", end = seperator)
+
+        except Exception as error:
+            print(error)
 
     def evaluate_model(self):
         """
@@ -160,15 +186,21 @@ def main():
     -------
     """
 
-    svc = SupportVectorClassifier("user_data.csv")
-    svc.load_data()
-    svc.check_data()
-    svc.preprocess_data()
-    svc.eda()
-    svc.split_data()
-    svc.transform_data()
-    svc.train_model()
-    svc.evaluate_model()
+    try:
+        svc = SupportVectorClassifier("user_data.csv")
+        svc.load_data()
+        svc.check_data()
+        svc.preprocess_data()
+        svc.eda()
+        svc.split_data()
+        svc.transform_data()
+        svc.train_model()
+        svc.evaluate_model()
+
+    except KeyboardInterrupt:
+        print("User wants to exit.")
+    except Exception as error:
+        print(error)
 
 if __name__ == "__main__":
     main()
