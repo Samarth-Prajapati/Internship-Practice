87c55336a7aee765ed19bfed5529c68d49d03edd
Samarth-Prajapati <samarthp475@gmail.com>
2026-03-10 11:29:13 +0530

    Update : Implemented method - Analyse Dataset.

--- 10032026_tuesday/KMeansClustering/main.py
+++ 10032026_tuesday/KMeansClustering/main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-seperator = f"\n\n{'----' * 70}\n\n"
+seperator = f"\n\n{'--' * 75}\n\n"
 
 class ClusteringAlgorithm:
     """Implementation of K Means Clustering Algorithm"""
@@ -15,18 +15,45 @@ class ClusteringAlgorithm:
         -------
         """
 
-        self.df = pd.read_csv("MallCustomers.csv")
-        print(f"\nDataset = \n{self.df.head()}\n")
+        try:
+            self.df = pd.read_csv("MallCustomers.csv")
+            print(f"\nDataset = \n{self.df.head()}\n")
 
-        print("Dataset Loaded Successfully.", end = seperator)
+            print("Dataset Loaded Successfully.", end = seperator)
 
+        except FileNotFoundError:
+            print("File not found.")
+        except Exception as error:
+            print(error)
 
+    def analyse_dataset(self):
+        """
+        Analysis of dataset to check null values and duplicates, necessary for preprocessing data
+        Returns - None
+        -------
+        """
+
+        self.load_dataset()
+
+        try:
+            print("Dataset Info = ")
+            self.df.info()
+
+            print(f"\nDataset Description = \n{self.df.describe()}\n")
+            print(f"Checking Null Values = \n{self.df.isnull().sum()}\n")
+            print(f"Sum of Duplicates = {self.df.duplicated().sum()}")
+            print(f"Dataset Shape = {self.df.shape}\n")
+
+            print("Dataset Analysis Successful.", end = seperator)
+
+        except Exception as error:
+            print(error)
 
 def main():
     """Main Function"""
 
     cluster = ClusteringAlgorithm()
-    cluster.load_dataset()
+    cluster.analyse_dataset()
 
 if __name__ == "__main__":
     main()
