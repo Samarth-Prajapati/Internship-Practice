b834f6b53a34ea1de8192eb17fd4b4b4360631dc
Samarth-Prajapati <samarthp475@gmail.com>
2026-03-25 17:05:20 +0530

    Add : configure llm, api_key and model

---
+++ 25032026_wednesday/MiniBA/config.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+
+load_dotenv()
+
+try:
+    GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+    GROQ_MODEL_2 = os.getenv("GROQ_MODEL_2")
+
+    llm = ChatGroq(
+        api_key = GROQ_API_KEY,
+        model = GROQ_MODEL_2,
+        temperature = 0.3
+    )
+
+except Exception as error:
+    print(error)
