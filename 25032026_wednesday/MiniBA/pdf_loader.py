77ca779afadc0958789126a142b79d55411c7793
Samarth-Prajapati <samarthp475@gmail.com>
2026-03-25 17:05:41 +0530

    Add : loads pdf for using agent

---
+++ 25032026_wednesday/MiniBA/pdf_loader.py
@@ -0,0 +1,27 @@
+from langchain_community.document_loaders import PyPDFLoader
+
+def load_pdf(file_path):
+    """
+    Load a PDF file
+    Parameters
+    ----------
+    file_path - file path
+
+    Returns - PDF text
+    -------
+    """
+
+    try:
+        loader = PyPDFLoader(file_path)
+        pages = loader.load()
+
+        text = ""
+        for page in pages:
+            text += page.page_content + "\n"
+
+        return text
+
+    except FileNotFoundError:
+        return "PDF not found"
+    except Exception as error:
+        return error
